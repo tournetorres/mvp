0ce1c7c61970b096b94dce4c365fc224eb18033f
tournetorres <tournetorres@gmail.com>
2017-06-19 23:38:05 -0500

    added some angular and templating

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-let myKey = config.MY_KEY;
+'use strict';
+
+// create the module and name it scotchApp
+angular.module('bookd8', ['ngRoute'])
+
+// create the controller and inject Angular's $scope
+.controller('mainController', ($scope) => {
+  // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+  })
+
+  .config(($routeProvider) => {
+    $routeProvider
+
+    // route for the home page
+      .when('/', {
+        templateUrl: '/home.html',
+        controller: 'mainController',
+      })
+
+    // route for the about page
+      .when('/about', {
+        templateUrl: '/about.html',
+        controller: 'aboutController',
+      })
+
+    // route for the contact page
+      .when('/contact', {
+        templateUrl: '/contact.html',
+        controller: 'contactController',
+      });
+  })
+
+
+// create the controller and inject Angular's $scope
+  .controller('mainController', ($scope) => {
+  // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+  })
+  .controller('aboutController', ($scope) => {
+  $scope.message = 'Look! I am an about page.';
+})
+
+.controller('contactController', ($scope) => {
+  $scope.message = 'Contact us! JK. This is just a demo.';
+});
+
